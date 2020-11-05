741bb2fcf66d1ce4e1f484d6f4e4ce4ed6c542a4
Piyushyadav1001 <72632167+Piyushyadav1001@users.noreply.github.com>
2020-11-06 04:24:41 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
-
+import React from "react";
+import "../styles/App.css";
+import Comp1 from "./Comp1";
+import Comp2 from "./Comp2";
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <>
+      <Comp1 />
+      <Comp2 />
+    </>
+  );
+};
 
 export default App;
---
+++ src/components/Comp2.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/App.css";
+
+const Comp2 = () => {
+  return (
+    <>
+      <h1>Nwes Website</h1>
+      <h3>
+        This project is about provide live news to tall the users based on their
+        favourite section(Sports,Bollywood,Politics).
+      </h3>
+    </>
+  );
+};
+
+export default Comp2;
